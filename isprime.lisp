6545e8c022756556bc01c1a11cdebf5c2c34e381
Dušan Radisavljević <ducaradisavljevic@gmail.com>
2018-01-28 13:43:43 +0100

    Add files via upload

---
+++ isprime.lisp
@@ -0,0 +1,5 @@
+(defun isprime (primenum) 
+(defvar numcheck) 
+(setf numcheck 0) 
+(loop for a from 1 to (- (floor (sqrt primenum)) 1) do (if (not (= a 1) ) (if (= (rem primenum a) 0) (setf numcheck 1) ) ) ) 
+(if (= numcheck 0) (return-from isprime t) (return-from isprime nil) ) )
